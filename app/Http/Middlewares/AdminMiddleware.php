c571bac57039d930d84b425aa5174b45189465f6
Kamil Kuźma <kamil.kuzma@madmountain.io>
2025-05-18 20:19:46 +0200

    Modify AdminMiddleware

--- app/Http/Middlewares/AdminMiddleware.php
+++ app/Http/Middlewares/AdminMiddleware.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Middlewares;
 
+use App\Models\User;
+
 class AdminMiddleware
 {
     public static function handle(): void
     {
-        if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
+        if (!isset($_SESSION['user']) || $_SESSION['user']->{User::ROLE} !== User::ROLE_ADMIN) {
             header('Location: /');
             exit;
         }
