d76cea299a67e1758d4f4a1e6c065b688d8566cb
Kamil Kuźma <kamil.kuzma@madmountain.io>
2025-06-06 17:37:21 +0200

    Check if user exists in database

--- app/Http/Middlewares/LoggedMiddleware.php
+++ app/Http/Middlewares/LoggedMiddleware.php
@@ -2,11 +2,26 @@
 
 namespace App\Http\Middlewares;
 
+use App\Repositories\UserRepository;
+
 class LoggedMiddleware
 {
     public static function handle(): void
     {
         if (!isset($_SESSION['user'])) {
+            header('Location: /login');
+            exit;
+        }
+
+        // Check if the user exists in the database
+        $userRepository = new UserRepository();
+        $user = $userRepository->findById($_SESSION['user']->getId());
+
+        if (!$user) {
+            // Clear the session if the user does not exist
+            session_destroy();
+            unset($_SESSION['user']);
+
             header('Location: /login');
             exit;
         }
